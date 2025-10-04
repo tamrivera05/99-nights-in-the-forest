eee2cfd43640a96a9b9fe6feea880967daa08ecf
tamrivera05 <tamangelarivera@gmail.com>
2025-10-05 02:54:21 +0800

    Integrate umami analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
-
+import umami from "@yeskunall/astro-umami";
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://timbuilding.com",
+  site: "https://99nightsintheforestfree.com",
   vite: {
     plugins: [tailwindcss()],
   },
@@ -21,6 +21,11 @@ export default defineConfig({
       priority: 0.7,
     }),
     alpinejs(),
+    umami({
+      id:"78d86786-079b-49e7-bb2f-0007e2c5e12a",
+      endpointUrl:"https://analytics.growagardenfree.com",
+      hostUrl:"https://analytics.growagardenfree.com",
+    }),
     robotsTxt(),
   ],
 });
